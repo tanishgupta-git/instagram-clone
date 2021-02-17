9b77cee25bc14a17c7ad4846e027e4b999f3be10
tanishgupta-git <tanishgupta8181@gmail.com>
2021-02-17 18:22:25 +0530

    changes for converting into pwa

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import * as serviceWorker from './serviceWorkerRegistration'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,6 +19,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorker.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
